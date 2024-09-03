eaf581df7c4e350bc8288e9e5d8cb08e94dd4133
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2024-09-03 23:24:47 +0530

    n

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
